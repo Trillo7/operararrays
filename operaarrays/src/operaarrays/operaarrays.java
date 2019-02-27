c242bc0a68462252ae4661a84a314cb5e93191ff
Trillo diurno <fr.trillo7@gmail.com>
2019-02-27 10:33:47 +0100

    subida array

---
+++ operaarrays/src/operaarrays/operaarrays.java
@@ -0,0 +1,15 @@
+package operaarrays;
+
+public class operaarrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array10=new int[10];
+	}
+	
+	public int rellenaarray(int[] array10) {
+		return 0;
+		
+	}
+
+}
