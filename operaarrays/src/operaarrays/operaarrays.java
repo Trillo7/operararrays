29667fb0f3112d93c7342d37d8ce7be93fb5c32e
Trillo diurno <fr.trillo7@gmail.com>
2019-03-06 09:48:56 +0100

    Rellena array

--- operaarrays/src/operaarrays/operaarrays.java
+++ operaarrays/src/operaarrays/operaarrays.java
@@ -5,10 +5,14 @@ public class operaarrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] array10=new int[10];
+		array10=rellenaarray(array10);
 	}
 	
-	public int rellenaarray(int[] array10) {
-		return 0;
+	public static int[] rellenaarray(int[] array10) {
+		for(int i=0;i<10;i++) {
+			array10[i]=(int) (Math.random() * (20 - 1) + 1);
+		}
+		return array10;
 		
 	}
 
